568881cdb989cc3e07addc6d61fada9e2d31fb04
Renan Franca <renan.andradefranca@gmail.com>
2022-07-29 16:55:48 -0300

    Initial version of baby generated by generator-jhipster@7.8.1

---
+++ src/test/java/com/mamazinha/baby/config/NoOpMailConfiguration.java
@@ -0,0 +1,25 @@
+package com.mamazinha.baby.config;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+
+import com.mamazinha.baby.service.MailService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class NoOpMailConfiguration {
+
+    private final MailService mockMailService;
+
+    public NoOpMailConfiguration() {
+        mockMailService = mock(MailService.class);
+        doNothing().when(mockMailService).sendActivationEmail(any());
+    }
+
+    @Bean
+    public MailService mailService() {
+        return mockMailService;
+    }
+}
