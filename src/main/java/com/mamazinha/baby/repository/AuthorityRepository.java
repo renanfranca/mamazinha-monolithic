58d36cfcd082eda6b58d05a93e4374259a19dea1
Renan Franca <renan.andradefranca@gmail.com>
2022-07-29 16:55:48 -0300

    Initial version of baby generated by generator-jhipster@7.8.1

---
+++ src/main/java/com/mamazinha/baby/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.mamazinha.baby.repository;
+
+import com.mamazinha.baby.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/com/mamazinha/baby/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.mamazinha.baby.web.rest.vm;
